4b2717e35b145e6d80211b55a017082fc108c917
skanehira <sho19921005@gmail.com>
2024-04-08 01:55:51 +0900

    chore: impl todo create and delete transactions

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,58 @@
 use goose::prelude::*;
+use serde::Deserialize;
 
-async fn loadtest_index(user: &mut GooseUser) -> TransactionResult {
-    let _goose_metrics = user.get("").await?;
+#[derive(Deserialize)]
+struct Todo {
+    id: u32,
+}
+
+struct Session {
+    id: u32,
+}
+
+async fn todo_create(user: &mut GooseUser) -> TransactionResult {
+    let todo = serde_json::json!({
+        "title": "Learn Rust",
+        "completed": false,
+    });
+
+    let mut goose_resp = user.post_json("/todos", &todo).await?;
+
+    match goose_resp.response {
+        Ok(resp) => match resp.json::<Todo>().await {
+            Ok(todo) => {
+                user.set_session_data(Session { id: todo.id });
+                Ok(())
+            }
+            Err(err) => {
+                return user.set_failure(
+                    "create todo",
+                    &mut goose_resp.request,
+                    None,
+                    Some(err.to_string().as_str()),
+                );
+            }
+        },
+        Err(err) => {
+            return user.set_failure(
+                "create todo",
+                &mut goose_resp.request,
+                None,
+                Some(err.to_string().as_str()),
+            );
+        }
+    }
+}
+
+async fn todo_delete(user: &mut GooseUser) -> TransactionResult {
+    let session = user.get_session_data_unchecked::<Session>();
+    let _ = user.delete(&format!("/todos/{}", session.id)).await?;
+
+    Ok(())
+}
+
+async fn todos(user: &mut GooseUser) -> TransactionResult {
+    let _ = user.get("/todos").await?;
 
     Ok(())
 }
@@ -10,8 +61,10 @@ async fn loadtest_index(user: &mut GooseUser) -> TransactionResult {
 async fn main() -> Result<(), GooseError> {
     GooseAttack::initialize()?
         .register_scenario(
-            scenario!("LoadtestTransactions")
-                .register_transaction(transaction!(loadtest_index).set_name("get index")),
+            scenario!("create and get todos")
+                .register_transaction(transaction!(todo_create).set_name("create todo"))
+                .register_transaction(transaction!(todos).set_name("get todos"))
+                .register_transaction(transaction!(todo_delete).set_name("delete todo")),
         )
         .execute()
         .await?;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,77 @@
-use axum::{routing::get, Router};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+#[derive(Clone, Serialize)]
+struct Todo {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+#[derive(Deserialize)]
+struct TodoCreateInput {
+    title: String,
+    completed: bool,
+}
+
+async fn todos_create(
+    State(state): State<Arc<Mutex<AppState>>>,
+    Json(input): Json<TodoCreateInput>,
+) -> impl IntoResponse {
+    let mut state = state.lock().unwrap();
+    let id = state.id + 1;
+    state.id = id;
+
+    let todo = Todo {
+        id: state.id,
+        title: input.title,
+        completed: input.completed,
+    };
+
+    state.todos.insert(id, todo.clone());
+    (StatusCode::CREATED, Json(todo))
+}
+
+async fn todos_delete(
+    State(state): State<Arc<Mutex<AppState>>>,
+    path: Path<u32>,
+) -> impl IntoResponse {
+    let mut state = state.lock().unwrap();
+    state.todos.remove(&path.0);
+    StatusCode::NO_CONTENT
+}
+
+async fn todos(State(state): State<Arc<Mutex<AppState>>>) -> impl IntoResponse {
+    Json(state.lock().unwrap().todos.clone())
+}
+
+struct AppState {
+    todos: HashMap<u32, Todo>,
+    id: u32,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let state = Arc::new(Mutex::new(AppState {
+        todos: HashMap::new(),
+        id: 0,
+    }));
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/todos", get(todos).post(todos_create))
+        .route("/todos/:id", delete(todos_delete))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
